f2fe0e782a2eb1c243e575cc62e73708713e0df7
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2024-06-27 01:00:14 +0200

    x

--- TestAutoPullWithGitHubAction/Composer.cs
+++ TestAutoPullWithGitHubAction/Composer.cs
@@ -20,10 +20,10 @@ namespace TestAutoPullWithGitHubAction
             process.Start();
             using (var writer = process.StandardInput)
             {
-                //writer.WriteLine("git init");
-                //writer.WriteLine($"git remote set-url origin https://KeyserDSoze:{githubToken}@github.com/KeyserDSoze/TestAutoPullWithGitHubAction.git");
+                writer.WriteLine("git init");
+                writer.WriteLine($"git remote set-url origin https://KeyserDSoze:{githubToken}@github.com/KeyserDSoze/TestAutoPullWithGitHubAction.git");
                 writer.WriteLine("git add .");
-                writer.WriteLine($"git commit -m \"new version v.{newVersion}\"");
+                writer.WriteLine($"git commit --author=\"alessandro rapiti <alessandro.rapiti44@gmail.com>\" -m \"new version v.{newVersion}\"");
                 writer.WriteLine("git push origin HEAD:master --force");
             }
             await process.WaitForExitAsync();
