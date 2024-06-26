e947626f0a7d027363020e77fe9aee772c7d3e1e
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2024-06-26 15:37:46 +0200

    Add project files.

---
+++ TestAutoPullWithGitHubAction/Composer.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace TestAutoPullWithGitHubAction
+{
+    internal class Composer
+    {
+        public async Task CommitAndPushAsync(string path, string newVersion, string? githubToken)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    WorkingDirectory = path
+                },
+            };
+
+            process.Start();
+            using (var writer = process.StandardInput)
+            {
+                if (githubToken != null)
+                    writer.WriteLine($"git remote set-url origin https://KeyserDSoze:{githubToken}@github.com/KeyserDSoze/TestAutoPullWithGitHubAction.git");
+                writer.WriteLine("git init");
+                writer.WriteLine("git add .");
+                writer.WriteLine($"git commit --author=\"alessandro rapiti <alessandro.rapiti44@gmail.com>\" -m \"new version v.{newVersion}\"");
+                writer.WriteLine("git push");
+            }
+            await process.WaitForExitAsync();
+        }
+    }
+}
---
+++ TestAutoPullWithGitHubAction/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestAutoPullWithGitHubAction;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var x = args.ToDictionary(x => x.Split('=')[0], x => x.Split('=')[1]);
+        var path = x["path"];
+        var newFile = $"{path}/newFile.txt";
+        var fileInfo = new FileInfo(newFile);
+        if (fileInfo.Exists)
+            fileInfo.Delete();
+        fileInfo.CreateText().Write(Guid.NewGuid().ToString());
+        new Composer().CommitAndPushAsync(path, x["v"], x["t"]).ConfigureAwait(false).GetAwaiter().GetResult();
+    }
+}
